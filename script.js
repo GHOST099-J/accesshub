fe7a7911837aa23981d43ad1f29365b8835de3b8
GHOST099-J <mjit21002@gmail.com>
2026-08-31 15:45:39 +0530

    Update Supabase URL in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://awtpelvnm...supabase.co";
+const SUPABASE_URL = "https://awtpelvnmpalmynouttg.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3dHBlbHZubXBhbG15bm91dHRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNTMwNDksImV4cCI6MjEwMzcyOTA0OX0.sdTA_oSM8G2nqd-v4-ZabDv83icdgL9JnAYJ09CGkOA";
 
 const db = supabase.createClient(
